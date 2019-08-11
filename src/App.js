20ad891d025ccf14c497917fedea62b2f2fbe965
Tai Pham <pktai.iot@gmail.com>
2019-08-11 22:00:58 +0700

    add element

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 // import logo from './logo.svg';
-// import './App.css';
-import { Button } from 'antd';
+import './App.css';
+import { Button, DatePicker } from 'antd';
+
+
+function onChange(date, dateString) {
+  console.log(date, dateString);
+}
 function App() {
   return (
     <div className="App">
       <Button
-      type="primary"
-      size="large"
+        type="primary"
+        size="large"
       >test</Button>
+      <DatePicker onChange={onChange} />
+      <br />
     </div>
   );
 }
